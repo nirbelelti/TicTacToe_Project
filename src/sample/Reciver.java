6b4b1784b6f7255cf7947d16d40fe9853a8f53d2
nirbelelti <nirbelelti@gmail.com>
2015-09-17 13:48:20 +0200

    Left to do: fix/implement receiver class in the controller

---
+++ src/sample/Reciver.java
@@ -0,0 +1,74 @@
+package sample;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
+/**
+ * Created by nirbelelti on 16/09/15.
+ */
+public class Reciver {
+
+
+
+        private static int LISTE_PORT = 9000;
+      //  private static final int SEND_PORT = 9000;
+        private static final String IP = "10.111.176.175";
+
+        public static void main(String[] args) throws Exception {
+            InetAddress IPAddress = InetAddress.getByName(IP);
+
+
+            DatagramSocket receiveSocket = new DatagramSocket(LISTE_PORT);
+
+
+            Thread receiveThread = new Thread(() ->
+            {
+                byte[] data = new byte[10];
+
+                while (true) {
+                    DatagramPacket receivePacket = new DatagramPacket(data, data.length);
+                    try {
+                        receiveSocket.receive(receivePacket);
+                        String msg = new String(receivePacket.getData());
+                        msg.trim();
+                        if(msg.length()>0) {
+                            System.out.println("Received:  " + msg);
+
+                            msg = null;
+                            data = new byte[10];
+
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+
+//
+//                Sender conf = new Sender();
+//
+//            try {
+//                   conf.sent(0 ,"O,K");
+//              } catch (Exception e) {
+//
+//              }
+                 //   break;
+                  }
+            });
+
+
+
+            receiveThread.start();
+
+        }
+
+    }
+
+
+
+
+
+
+
---
+++ src/sample/Sender.java
@@ -0,0 +1,59 @@
+package sample;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
+/**
+ * Created by nirbelelti on 16/09/15.
+ */
+public class Sender  {
+
+      //  private static int LISTE_PORT = 9001;
+        private static final int SEND_PORT = 9000;
+        private static final String IP = "10.111.176.175";
+
+
+
+
+
+    public static void sent(Integer fildSent,String  playerSent) throws Exception {
+
+
+
+        String msg = (""+fildSent+"," +playerSent);
+
+        InetAddress IPAddress = InetAddress.getByName(IP);
+
+            DatagramSocket sendSocket = new DatagramSocket();
+
+
+            Thread sendThread = new Thread(() ->
+            {
+                byte[] data = new byte[1024];
+
+
+
+            //   while (true) {
+
+
+                    data = msg.getBytes();
+                    DatagramPacket sendPacket = new DatagramPacket(data, data.length, IPAddress, SEND_PORT);
+                    try {
+                        sendSocket.send(sendPacket);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+             //   }
+            });
+
+
+            sendThread.start();
+
+
+        }
+
+    }
+
